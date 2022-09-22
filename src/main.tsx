852fe0ba848c1f86d5a724ff729ca82571a829f9
Jessica Severino <jessicaa.severinoo@gmail.com>
2022-09-22 20:34:20 -0300

    feature page login

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { routes } from './routes/Routes';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { routes } from "./routes/Routes";
+import "antd/dist/antd.css";
+import "./styles.scss";
 
-import {
-  RouterProvider,
-} from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 
 const router = routes;
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
