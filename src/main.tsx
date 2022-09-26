e78045c29418cf5a77dc26ade4844168af2a8c55
Jessica Severino <jessicaa.severinoo@gmail.com>
2022-09-26 20:51:58 -0300

    remove file

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { routes } from "./routes/Routes";
 import "antd/dist/antd.css";
-import "./styles.scss";
 
 import { RouterProvider } from "react-router-dom";
 
